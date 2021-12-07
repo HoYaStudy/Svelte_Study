02e38e01d31f9a4a53174d0cfd06ce28acba9392
llHoYall <hoya128@gmail.com>
2021-12-07 22:59:01 +0900

    Added Getting-Started

---
+++ getting-started/src/routes/index.svelte
@@ -0,0 +1,5 @@
+<h1 class="m-4 text-3xl font-bold">Welcome to SvelteKit</h1>
+<p class="ml-4 text-md">
+	Visit <a href="https://kit.svelte.dev" class="text-purple-700">kit.svelte.dev</a> to read the documentation
+</p>
+<button class="btn">Button Example</button>
